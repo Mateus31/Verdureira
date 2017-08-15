4b9dd5fe747650251771d9146a2916b8fa5a014e
Mateus31 <mateus.machado@catolicasc.org.br>
2017-08-15 20:43:55 -0300

    Add project files.

---
+++ WebApplication1/Controllers/ClientesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ClientesController : Controller
+    {
+        //
+        // GET: /Cliente/
+        public ActionResult Index()
+        {
+            var cliente = new Cliente
+            {
+                Nome = "José"
+            };
+            return View(cliente);
+        }
+	}
+}
---
+++ WebApplication1/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Item
+
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+
+        public string Tipo { get; set; }
+    }
+    
+
+}
